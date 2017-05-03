b3a70432bc078bd136d1514f9c9326e20baf7ab2
daicorrea <daiane.ocorrea@gmail.com>
2017-05-03 12:06:59 -0300

    Adding Javascript Collapse Function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(".aboutTitle").click(function () {
+    console.log("oi");
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+});
