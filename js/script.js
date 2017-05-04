f0f7a06a85da108bf29348f813b4a0a9f3df8e53
daicorrea <daiane.ocorrea@gmail.com>
2017-05-04 12:45:45 -0300

    Collapsing All Sections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 $(".aboutTitle").click(function () {
-    console.log("oi");
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+});
+
+$(".startingGit").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+});
+
+$(".startingProject").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+});
+
+$(".creatingBranches").click(function () {
     $header = $(this);
     //getting the next element
     $content = $header.next();
